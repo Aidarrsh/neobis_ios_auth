552acdbf90b9380750151ef7ba6611593a3be5f4
aidar <sharipov_ai@auca.kg>
2023-06-01 14:57:55 +0600

    Finished splashScreen

---
+++ neobis_ios_auth/ViewModels/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  neobis_ios_auth
+//
+//  Created by Айдар Шарипов on 28/5/23.
+//
+
+import Foundation
---
+++ neobis_ios_auth/Views/SplashScreen/SplashViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  neobis_ios_auth
+//
+//  Created by Айдар Шарипов on 25/5/23.
+//
+
+import UIKit
+import SnapKit
+
+class SplashViewController : UIViewController {
+    
+    let mainView = SplashScreenView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        mainView.authorizeButton.addTarget(self, action: #selector(authorizeButtonPressed), for: .touchUpInside)
+        mainView.beginButton.addTarget(self, action: #selector(beginButtonPressed), for: .touchUpInside)
+    }
+    
+    func setupView(){
+        view.addSubview(mainView)
+        
+        mainView.snp.makeConstraints{ make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    @objc func beginButtonPressed() {
+        let vc = RegistrationViewController()
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func authorizeButtonPressed() {
+        let vc = LoginViewController()
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
